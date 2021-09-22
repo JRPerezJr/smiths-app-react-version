1646cd1d221b001a18125f2b32018022581c40a0
Juan <pront23@gmail.com>
2021-09-22 23:56:30 +0900

    updated file paths

---
+++ src/data/musicDB.data.js
@@ -0,0 +1,96 @@
+const baseCoverFolder = '../assets';
+const musicDataBase = {
+  artists: {
+    'The Smiths': {
+      albums: {
+        'The Queen is Dead': {
+          tracks: [
+            {
+              id: 1,
+              fileName: '01-TheSmiths-TheQueenIsDead',
+              displayName:
+                'The Queen Is Dead(Take Me Back To Dear Old Blightly Medley)',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: `${baseCoverFolder}/the_queen_is_dead.jpg`,
+            },
+            {
+              id: 2,
+              fileName: '02-The Smiths-FranklyMrShankly',
+              displayName: 'Frankly, Mr. Shankly',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'the_queen_is_dead',
+            },
+            {
+              id: 3,
+              fileName: '03-TheSmiths-IKnowItsOver',
+              displayName: "I Know It's Over",
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'the_queen_is_dead',
+            },
+            {
+              id: 4,
+              fileName: '04-TheSmiths-NeverHadNoOneEver',
+              displayName: 'Never Had No One Ever',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'the_queen_is_dead',
+            },
+            {
+              id: 5,
+              fileName: '05-TheSmiths-CemetryGates',
+              displayName: 'Cemetry Gates',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'queen_is_dead2',
+            },
+            {
+              id: 6,
+              fileName: '06-TheSmiths-BigmouthStrikesAgain',
+              displayName: 'Bigmouth Strikes Again',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'queen_is_dead2',
+            },
+            {
+              id: 7,
+              fileName: '07-TheSmiths-TheBoyWithTheThornInHisSide',
+              displayName: 'The Boy With The Torn In His Side',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'queen_is_dead2',
+            },
+            {
+              id: 8,
+              fileName: '08-TheSmiths-VicarInATutu',
+              displayName: 'Vicar In A Tutu',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'queen_is_dead2',
+            },
+            {
+              id: 9,
+              fileName: '09-TheSmiths-ThereIsALightThatNeverGoesOut',
+              displayName: 'There Is A Light That Never Goes Out',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'queen_is_dead2',
+            },
+            {
+              id: 10,
+              fileName: '10-TheSmiths-SomeGirlsAreBiggerThanOthers',
+              displayName: 'Some Girls Are Bigger Than Others',
+              artist: 'The Smiths',
+              album: 'The Queen Is Dead',
+              albumCover: 'queen_is_dead2',
+            },
+          ],
+        },
+      },
+    },
+  },
+};
+
+export default musicDataBase;

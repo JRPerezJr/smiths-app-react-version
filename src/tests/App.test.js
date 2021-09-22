fd1a1987b594ab57b8cf7816f5853dfc51cab8a0
Juan <pront23@gmail.com>
2021-09-22 11:03:18 +0900

    prepared test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,27 @@
-import React from 'react';
+// import React from 'react';
 
-import renderer from 'react-test-renderer';
+// import {render, screen} from '@testing-library/react'
 
-// import { mount } from 'enzyme';
-
-import App from '../App';
+// import App from '../App';
 
 describe('App component tests', () => {
   it('Sanity test for App component', () => {
     console.log('App component sanity test passed!');
   });
 
-  test('snapshot renders without errors', () => {
-    const component = renderer.create(<App />);
-    let tree = component.toJSON();
+  // test('snapshot renders without errors', () => {
 
-    expect(tree).toMatchSnapshot();
-  });
+  // });
 
-  // test('renders main title', () => {
+  // test('renders player container', () => {
+
+  // });
+
+  // test('renders theme button', () => {
+
+  // });
+
+  // test('clicking theme button changes background color', () => {
 
   // });
 });

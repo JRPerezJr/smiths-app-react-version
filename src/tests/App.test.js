62da34006559e430c6b98a0746589c922d38e145
Juan <pront23@gmail.com>
2021-09-22 23:57:44 +0900

    updated tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,27 +1,55 @@
-// import React from 'react';
+import React from 'react';
 
-// import {render, screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-// import App from '../App';
+import App from '../App';
 
 describe('App component tests', () => {
   it('Sanity test for App component', () => {
     console.log('App component sanity test passed!');
   });
 
-  // test('snapshot renders without errors', () => {
+  test('has theme text Light Mode', () => {
+    render(<App />);
+    const spanNode = screen.getByText(/light mode/i);
+    expect(spanNode).toHaveTextContent('Light Mode');
+  });
 
-  // });
+  test('theme button should be unchecked on first load only', () => {
+    render(<App />);
 
-  // test('renders player container', () => {
+    const themeButton = screen.getByLabelText('theme-switch');
 
-  // });
+    expect(themeButton).toMatchObject({ checked: false });
+  });
 
-  // test('renders theme button', () => {
+  test('theme button changes text to Dark Mode', () => {
+    render(<App />);
 
-  // });
+    const themeButton = screen.getByLabelText('theme-switch');
 
-  // test('clicking theme button changes background color', () => {
+    userEvent.click(themeButton);
+    const spanNode = screen.getByText(/dark mode/i);
 
-  // });
+    expect(spanNode).toHaveTextContent('Dark Mode');
+  });
+
+  test('theme button changes to checked true', () => {
+    render(<App />);
+
+    const themeButton = screen.getByLabelText('theme-switch');
+
+    expect(themeButton).toMatchObject({ checked: true });
+  });
+
+  test('should render Album cover', () => {
+    render(<App />);
+
+    const albumCover = screen.getByAltText(/queen is dead.*? album cover/i);
+
+    expect(albumCover).toMatchObject({
+      alt: 'queen is dead album cover',
+    });
+  });
 });

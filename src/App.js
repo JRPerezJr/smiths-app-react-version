30b96f99da94bc7e0153adea56b7915a89631ab7
Juan <pront23@gmail.com>
2021-09-28 20:37:39 +0900

    relocated data to app component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PlayerContainerComponent from './components/player-container/player-container';
 
 import ThemeSwitch from './components/theme-switch/theme-switch';
+import TRACKS from './data/music.data';
 
 function App() {
   return (
     <>
       <ThemeSwitch />
-      <PlayerContainerComponent />
+      <PlayerContainerComponent tracks={TRACKS} />
     </>
   );
 }
